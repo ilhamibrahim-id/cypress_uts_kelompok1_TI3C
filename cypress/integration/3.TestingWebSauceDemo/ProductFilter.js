2a78aa9ea79a2594889773530a003be7cd3587bd
Tita Wijayanti <wijayantitita094@gmail.com>
2021-10-29 13:41:53 +0700

    TC_PRODUCTFILTER_002 (PASS)

--- cypress/integration/3.TestingWebSauceDemo/ProductFilter.js
+++ cypress/integration/3.TestingWebSauceDemo/ProductFilter.js
@@ -6,5 +6,13 @@ describe('TC_PRODUCTFILTER | 1941720053 - Tita Wijayanti', () => {
         cy.get('#login-button').click();
         cy.get('[data-test=\"product_sort_container"\]').select("az");
         cy.get('.inventory_item_name:eq(0)').should('have.text', "Sauce Labs Backpack");
-      })
+    })
+    it('TC_PRODUCTFILTER_002', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("standard_user");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        cy.get('[data-test=\"product_sort_container"\]').select("za");
+        cy.get('.inventory_item_name:eq(0)').should('have.text', "Test.allTheThings() T-Shirt (Red)");
+    })
 })
