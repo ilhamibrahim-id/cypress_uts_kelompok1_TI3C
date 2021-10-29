fb0c7431f0de2642767e88067917c589a03f3893
Ilham Ibrahim <54558904+ilhamibrahim-id@users.noreply.github.com>
2021-10-29 09:46:56 +0700

    TC_ADDCART_004 (PASS)

--- cypress/integration/3.TestingWebSauceDemo/AddCart.js
+++ cypress/integration/3.TestingWebSauceDemo/AddCart.js
@@ -44,4 +44,18 @@ describe('TC_ADDCART_001 | 1941720180 - ILHAM IBRAHIM', () => {
       cy.get('#continue').click();
       cy.contains('Error: Last Name is required').should('be.visible');
     })
+    it('TC_ADDCART_004', () => {
+      cy.visit('/');
+      cy.get('#user-name').type("standard_user");
+      cy.get('#password').type("secret_sauce");
+      cy.get('#login-button').click();
+      cy.get('#add-to-cart-sauce-labs-backpack').click();
+      cy.get('#add-to-cart-sauce-labs-bike-light').click();
+      cy.get('.shopping_cart_link').click();
+      cy.contains('Checkout').click();  
+      cy.get('#first-name').type("Ilham");
+      cy.get('#last-name').type("Ibrahim");
+      cy.get('#continue').click();
+      cy.contains('Error: Postal Code is required').should('be.visible');
+    })
   })
