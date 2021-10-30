0d38f265cee76de38ba1d085b5a9b8a84c0e33f1
vegaangga <vegaangga27@gmail.com>
2021-10-30 16:17:58 +0700

    TC_SideBar_002 (PASS)

--- cypress/integration/3.TestingWebSauceDemo/SideBar.js
+++ cypress/integration/3.TestingWebSauceDemo/SideBar.js
@@ -19,4 +19,20 @@ describe('TC_SIDEBAR| 2041723013 - VEGA ANGGARESTA', function() {
 
     })
 
+    it('TC_SIDEBAR_002', function(){
+        cy.visit('/')
+
+        cy.get('[data-test=username]').type('standard_user')
+
+        cy.get('[data-test=password]').type('secret_sauce')
+
+        cy.get('[data-test=login-button]').click()
+
+        cy.get('#react-burger-menu-btn').click()
+
+        cy.get('#about_sidebar_link').click()
+
+        cy.get('.is-full-mobile > .content-container > .title').should('be.visible')
+    })
+
 })
