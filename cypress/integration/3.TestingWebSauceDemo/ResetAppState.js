da9259362b12a7e7a54cfb19ac0b3c992e2a7eb2
ShelycaSurrayensih <shelysurrayensih0501@gmail.com>
2021-10-31 21:24:27 +0700

    TC_ResetAppState_003(PASS)

--- cypress/integration/3.TestingWebSauceDemo/ResetAppState.js
+++ cypress/integration/3.TestingWebSauceDemo/ResetAppState.js
@@ -23,4 +23,16 @@ describe('TC_ResetAppState| 21941720021 - Shelyca Surrayensih', function() {
         cy.get('#reset_sidebar_link').should('be.visible').click()
 
     })
+    
+    it('TC_ResetAppState_003 ', function(){
+        cy.visit('/')
+    
+    cy.get('#user-name').type('problem_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').should('be.visible').click()
+    cy.get('#add-to-cart-sauce-labs-backpack').should('be.visible').click()
+    cy.get('#react-burger-menu-btn').should('be.visible').click()
+    cy.get('#reset_sidebar_link').should('be.visible').click()
+    
+    })
 })
