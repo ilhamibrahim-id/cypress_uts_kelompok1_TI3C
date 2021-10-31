38120d7a7f50119f8d9cabdafd7336f3dfda3c62
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-10-31 22:13:57 +0700

    Update test case login

---
+++ cypress/integration/3.TestingWebSauceDemo/Login.js
@@ -0,0 +1,44 @@
+describe('TS_LOGIN | 1941720065 - AWANG SYUKRIANSAH DIRGANTORO', () => {
+    it('TS_LOGIN_001', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("standard_user");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        cy.contains('PRODUCTS').should('be.visible');
+    })
+    it('TS_LOGIN_002', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("problem_user");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        cy.get('.inventory_item_img:eq(0)').find('img').should('have.attr', 'src').should('include', 'sauce-backpack-1200x1500.34e7aa42');
+    })
+    it('TS_LOGIN_003', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("locked_out_user");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        cy.contains('PRODUCTS').should('be.visible');
+    })
+    it('TS_LOGIN_004', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("performance_glitch_user");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        cy.contains('PRODUCTS').should('be.visible');
+    })
+    it('TS_LOGIN_005', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("admin");
+        cy.get('#password').type("admin");
+        cy.get('#login-button').click();
+        cy.contains('Epic sadface: Username and password do not match any user in this service').should('be.visible');
+    })
+    it('TS_LOGIN_006', () => {
+        cy.visit('/');
+        cy.get('#user-name').type("admin");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        cy.contains('Epic sadface: Username do not match any user in this service').should('be.visible');
+    })
+})
